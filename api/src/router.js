2230b44e7a6bd030abf9f83d4a9234672f436c9a
Daniel Simpson <daniel_simpson@gap.com>
2022-04-07 15:21:31 -0700

    module 3 done

--- api/src/router.js
+++ api/src/router.js
@@ -1,5 +1,9 @@
 const {Router} = require('express')
+const { sendFile } = require('express/lib/response')
 const multer = require('multer')
+const path = require('path')
+
+const photoPath =  path.resolve(__dirname, '../../client/photo-viewer.html')
 
 const router = Router()
 
@@ -34,6 +38,10 @@ router.post('/upload', upload.single('photo'), (request,response) => {
     else{
         return response.status(201).json({success: true})
     }
+})
+
+router.get('/photo-viewer', (req, res) =>{
+    res.sendFile(photoPath)
 })
 
 module.exports = router;
